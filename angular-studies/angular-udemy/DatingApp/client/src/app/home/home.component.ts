d8d2a14aa3a2c758ee98fc2cde444dad18671313
Alex Andrade <axll@live.com>
2023-10-10 21:06:19 -0300

    pausa no curso udemy

--- angular-studies/angular-udemy/DatingApp/client/src/app/home/home.component.ts
+++ angular-studies/angular-udemy/DatingApp/client/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
+  currentUser$: Observable< User | null> = of(null);
   registerMode = false;
   users: any;
 
-  constructor( private http: HttpClient ) {  }
+  constructor( private http: HttpClient, public accountService : AccountService ) {  }
   
   ngOnInit(): void 
   {
+    this.currentUser$ = this.accountService.currentUser$;
     this.getUsers();
   }
 
