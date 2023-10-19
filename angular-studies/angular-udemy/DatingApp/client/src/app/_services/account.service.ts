e385414f75dc1bbda2fc01c77f2abd61a03ce5b4
Alex Andrade <axll@live.com>
2023-10-18 23:15:58 -0300

    Add logged user avatar temp

--- angular-studies/angular-udemy/DatingApp/client/src/app/_services/account.service.ts
+++ angular-studies/angular-udemy/DatingApp/client/src/app/_services/account.service.ts
@@ -27,6 +27,7 @@ export class AccountService {
         const usr = member;
         if (usr) {
           localStorage.setItem('user', JSON.stringify(usr));
+          console.log(usr);
           this.currentUserSource.next(usr);
         }
       })
